6fd80f17eebe24284c6481dbbe2c3313eec06f55
Yasir Anshori <yasiranshori72@gmail.com>
2022-12-31 13:20:14 +0700

    feat: migration and seeder files

---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Movie;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $movies = [
+            [
+                'name' => 'The Man in the bucket',
+                'slug' => 'the-man-in-the-bucket',
+                'category' => 'Action & Relations',
+                'video_url' => 'https://youtu.be/gDkrQp-zQCU',
+                'thumbnail' => 'https://placeimg.com/640/480/any',
+                'rating' => 9.6,
+                'is_featured' => 1,
+            ],
+            [
+                'name' => 'The girl in the dipper',
+                'slug' => 'the-girl-in-the-dipper',
+                'category' => 'Love & Funny',
+                'video_url' => 'https://youtu.be/gDkrQp-zQCU',
+                'thumbnail' => 'https://placeimg.com/640/480/animals',
+                'rating' => 9.2,
+                'is_featured' => 0,
+
+            ],
+            [
+                'name' => 'The girl in the dipper II',
+                'slug' => 'the-girl-in-the-dipper-II',
+                'category' => 'Love & Funny',
+                'video_url' => 'https://youtu.be/gDkrQp-zQCU',
+                'thumbnail' => 'https://placeimg.com/640/480/animals',
+                'rating' => 5.2,
+                'is_featured' => 0,
+            ],
+        ];
+
+        Movie::insert($movies);
+    }
+}
